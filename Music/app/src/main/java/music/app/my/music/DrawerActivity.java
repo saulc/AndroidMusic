b6dbf42216fdd7547934f6a2b3e763aa3fd562d6
Saul <saulc@users.noreply.github.com>
2026-08-30 14:19:31 -0700

    basic test add
    bottom banner.

--- Music/app/src/main/java/music/app/my/music/DrawerActivity.java
+++ Music/app/src/main/java/music/app/my/music/DrawerActivity.java
@@ -56,6 +56,9 @@ import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
@@ -225,6 +228,8 @@ public class DrawerActivity extends AppCompatActivity
 
     private MediaSession mediaSession;
 
+    private AdView mAdView;
+
   //  private NotificationHelper noti;
 
     /* -----------------------------------   onCreate start.   ----------------------------------- */
@@ -385,6 +390,13 @@ public class DrawerActivity extends AppCompatActivity
         handleSearchIntents();
 
         addBattListener();
+
+        MobileAds.initialize(this, initializationStatus -> {
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
 
